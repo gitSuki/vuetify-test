6f7ca415b3bb2011a974bfbbd6b0b87f778344d2
gitSuki <collinbrown226@gmail.com>
2022-10-09 15:03:49 -0500

    Complete sidebar component

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,3 +1,26 @@
 <template>
-   <v-navigation-drawer expand-on-hover rail>hello</v-navigation-drawer>
+  <v-navigation-drawer expand-on-hover rail>
+    <v-list>
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+        title="Sandra Adams"
+        subtitle="sandra_a88@gmailcom"
+      ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="comfortable" nav>
+      <v-list-item
+        prepend-icon="mdi-image"
+        title="Color Gallery"
+        value="color"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-dialpad"
+        title="Grayspace Gallery"
+        value="grayspace"
+      ></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
