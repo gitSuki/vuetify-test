39d31f11b6dc739cc651a3fc471e3db3fc85ecf3
gitSuki <collinbrown226@gmail.com>
2022-10-09 15:58:50 -0500

    Add background card for gallery

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,22 +1,24 @@
 <template>
-  <v-row>
-    <v-col v-for="n in 200" :key="n" cols="4" sm="2">
-      <v-card></v-card>
-      <v-img
-        :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-        aspect-ratio="1"
-        cover
-      >
-        <template v-slot:placeholder>
-          <v-row class="fill-height ma-0" align="center" justify="center">
-            <v-progress-circular
-              indeterminate
-              color="grey-lighten-5"
-            ></v-progress-circular
-          ></v-row>
-        </template>
-      </v-img>
-    </v-col>
-  </v-row>
+  <v-card class="mx-5 my-3 pa-3">
+    <v-row>
+      <v-col v-for="n in 200" :key="n" cols="4" sm="2">
+        <v-card></v-card>
+        <v-img
+          :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+          :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+          aspect-ratio="1"
+          cover
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey-lighten-5"
+              ></v-progress-circular
+            ></v-row>
+          </template>
+        </v-img>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
